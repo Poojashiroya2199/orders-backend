05fc750af7eaac8e80a2bed3248f39a035b960e5
PoojaKorat <pkorat@cisco.com>
2024-02-16 00:50:22 +0530

    header update

--- index.js
+++ index.js
@@ -18,6 +18,14 @@ app.use(bodyParser.json());
 //     preflightContinue: false
 //   })
 // );
+
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', 'https://orders-frontend.netlify.app'); // Replace '*' with the specific origin you want to allow
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  next();
+});
+
 app.use(cors());
 
 mongoose
