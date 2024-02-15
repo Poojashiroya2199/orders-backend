6aa5ffa2ce967d173f74555f22381a29b72d955e
PoojaKorat <pkorat@cisco.com>
2024-02-16 01:05:22 +0530

    origin change

--- index.js
+++ index.js
@@ -9,25 +9,24 @@ const app = express();
 const PORT = 3001;
 
 app.use(bodyParser.json());
-app.use(
-  cors({
-    origin: "*", // Replace with your frontend URL
-    methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
-    credentials: true,
-    optionsSuccessStatus: 200,
-    preflightContinue: false
-  })
-);
 
-app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*'); // Replace '*' with the specific origin you want to allow
-  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-  res.header('')
-  next();
-});
+const corsOptions = {
+  origin: 'https://orders-frontend.netlify.app',
+  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+  credentials: true,  // Allow credentials (cookies, HTTP authentication) to be included
+  optionsSuccessStatus: 204, // Some legacy browsers (IE11, various SmartTVs) choke on 204
+};
 
-// app.use(cors());
+app.use(cors(corsOptions));
+
+// app.use((req, res, next) => {
+//   res.header('Access-Control-Allow-Origin', 'https://orders-frontend.netlify.app'); // Replace 'https://orders-frontend.netlify.app' with the specific origin you want to allow
+//   res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+//   res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+//   res.header('')
+//   next();
+// });
+
 
 mongoose
   .connect(
